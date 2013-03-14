a6ea348783fe9fcbe45e174c1de7bc8423bb2945
AKBelani <Abelani@mit.edu>
2013-03-13 20:19:49 -0400

    fuckit`

---
+++ launch-two.sh
@@ -0,0 +1,57 @@
+#!/bin/bash 
+#-------------------------------------------------------
+#  Part 1: Check for and handle command-line arguments
+#-------------------------------------------------------
+TIME_WARP=1
+JUST_MAKE="no"
+VNAME1="betty"
+for ARGI; do
+    if [ "${ARGI}" = "--help" -o "${ARGI}" = "-h" ] ; then
+	printf "%s [SWITCHES] [time_warp]   \n" $0
+	printf "  --just_make, -j    \n" 
+	printf "  --help, -h         \n" 
+	exit 0;
+    elif [ "${ARGI//[^a-z]/}" = "$ARGI" ] ; then
+	VNAME1=$ARGI
+    elif [ "${ARGI//[^0-9]/}" = "$ARGI" -a "$TIME_WARP" = 1 ]; then 
+        TIME_WARP=$ARGI
+    elif [ "${ARGI}" = "--just_build" -o "${ARGI}" = "-j" ] ; then
+	JUST_MAKE="yes"
+    else 
+	printf "Bad Argument: %s \n" $ARGI
+	exit 0
+    fi
+done
+
+#-------------------------------------------------------
+#  Part 2: Create the .moos and .bhv files. 
+#-------------------------------------------------------
+#VNAME1="archie"    # The first  vehicle community
+START_POS1="0,0"  
+SHORE_LISTEN="9300"
+
+nsplug meta_vehicle.moos targ_$VNAME1.moos -f WARP=$TIME_WARP  \
+   VNAME=$VNAME1      START_POS=$START_POS1                  \
+   VPORT="9001"       SHARE_LISTEN="9301"                    \
+   VTYPE=UUV          SHORE_LISTEN=$SHORE_LISTEN
+
+nsplug meta_vehicle.bhv targ_$VNAME1.bhv -f VNAME=$VNAME1      \
+    START_POS=$START_POS1 
+
+if [ ${JUST_MAKE} = "yes" ] ; then
+    exit 0
+fi
+
+#-------------------------------------------------------
+#  Part 3: Launch the processes
+#-------------------------------------------------------
+printf "Launching $VNAME1 MOOS Community (WARP=%s) \n" $TIME_WARP
+pAntler targ_$VNAME1.moos >& /dev/null &
+printf "Done \n"
+
+uMAC targ_$VNAME1.moos
+
+printf "Killing all processes ... \n"
+kill %1
+printf "Done killing processes.   \n"
+
